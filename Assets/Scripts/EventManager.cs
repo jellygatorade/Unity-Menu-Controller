0bb0a9e19b69f5a09c3036705489e3cf89f662bb
Kevin Kane <kkane@ncartmuseum.org>
2023-07-06 16:53:37 -0400

    add usage notes

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -8,6 +8,33 @@ using UnityEngine;
 
 // C# static modifer tutorial example
 // https://www.youtube.com/watch?v=8xcIy9cV-6g
+// 
+// The EventManager class methods can be used without instantiating an object 
+// because they are static methods, owned by the class
+// (not instance methods, owned per class instance)
+
+/*
+How to use
+
+Triggering events
+
+-- No parameter
+      EventManager.TriggerEvent("gameOver", null);
+-- 1 parameter
+      EventManager.TriggerEvent("gamePause", new Dictionary<string, object> { { "pause", true } });
+-- 2 or more parameters
+      EventManager.TriggerEvent("addReward", 
+        new Dictionary<string, object> {
+          { "name", "candy" },
+          { "amount", 5 } 
+        });
+
+Listening to events
+
+  EventManager.AddListener("addCoins", OnAddCoins);
+  EventManager.RemoveListener("addCoins", OnAddCoins);
+
+*/
 
 public class EventManager : MonoBehaviour {
   private Dictionary<string, Action<Dictionary<string, object>>> eventDictionary;
